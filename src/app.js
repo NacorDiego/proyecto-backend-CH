5ae275fd69bd0fcb2361dc3fbc169d869c50e70d
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-11-28 13:43:41 -0300

    Se instala express-handlebars.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,12 @@
 import server from './server.js'
 import config from './configs/config.js'
 import { connectDB } from './configs/dbConnection.js'
+import { engine } from 'express-handlebars'
 
+server.engine('handlebars', engine())
+server.set('views', process.cwd() + '/src/views')
+server.set('view engine', 'handlebars')
+server.use(express.static(process.cwd() + '/public'))
 
 server.listen(config.PORT, () => console.log(`Server is running at http://localhost:${config.PORT}`))
 connectDB()
