c8c2ff8f003f1d2d7ce88b8b5d281e06d4884b19
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2023-11-29 10:53:43 -0300

    Se crea MessageModel

---
+++ src/models/MessageModel.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import { Schema, model } from 'mongoose'
+
+const MessageSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export const Message = model('Message', MessageSchema)
